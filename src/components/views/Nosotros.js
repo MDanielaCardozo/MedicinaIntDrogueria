3017b0c1367633e12fa907ea872c38896c316b4d
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-07-18 23:11:56 -0300

    Agregué section comercializa

--- src/components/views/Nosotros.js
+++ src/components/views/Nosotros.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "../views/Nosotros.css";
 import { Col, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCrutch,
+  faHandshake,
+  faPills,
+  faRoute,
+  faShuffle,
+  faSyringe,
+  faVial,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Nosotros = () => {
   return (
     <div className="bgSection">
       <section className="portadaNosotros d-flex justify-content-end align-items-end p-3">
         <div className="textCarousel">
-          {/* <h1>Nosotros</h1> */}
           <p>
             DROGUERÍA MEDICINA INTEGRAL SRL llevamos más de 30 años
             <br />
@@ -52,7 +61,69 @@ const Nosotros = () => {
           </div>
         </div>
       </article>
-      <section className="container py-5">
+      <section className="container nosotros">
+        <div className="p-3">
+          <h2>Nuestra empresa comercializa</h2>
+          <hr />
+          <Row className="w-100 mt-5 mb-3">
+            <Col sm={12} md={6} lg={6}>
+              <div className="cardInfoNosotros w-100 p-2">
+                <div className="icono">
+                  <FontAwesomeIcon icon={faPills} className="iconoNos" />
+                </div>
+                <h3>Medicamentos</h3>
+                <p className="m-3">
+                  Con las nuevas ideas, pero manteniendo los mismos ideales.
+                  Estamos profundamente comprometidos con la innovación y el
+                  crecimiento permanente.
+                </p>
+              </div>
+            </Col>
+            <Col sm={12} md={6} lg={6}>
+              <div className="cardInfoNosotros w-100 p-2">
+                <div className="icono">
+                  <FontAwesomeIcon icon={faSyringe} className="iconoNos" />
+                </div>
+                <h3>Descartables en general</h3>
+                <p className="m-3">
+                  Nos adaptamos a las diversas circunstancias que se nos
+                  presentan dando respuestas ágiles, porque tomamos los cambios
+                  como nuevos desafíos por alcanzar.
+                </p>
+              </div>
+            </Col>
+          </Row>
+          <Row className="w-100 mb-5">
+            <Col sm={12} md={6} lg={6}>
+              <div className="cardInfoNosotros w-100 p-2">
+                <div className="icono">
+                  <FontAwesomeIcon icon={faCrutch} className="iconoNos" />
+                </div>
+                <h3>Nutrición Enteral</h3>
+                <p className="m-3">
+                  Constante para llegar más lejos siempre con nuestros clientes
+                  y proovedores, con el objetivo de ir mejorando continuamente y
+                  ser siempre los primeros.
+                </p>
+              </div>
+            </Col>
+            <Col sm={12} md={6} lg={6}>
+              <div className="cardInfoNosotros w-100 p-2">
+                <div className="icono">
+                  <FontAwesomeIcon icon={faVial} className="iconoNos" />
+                </div>
+                <h3>Hemoderivados</h3>
+                <p className="m-3">
+                  Constante para llegar más lejos siempre con nuestros clientes
+                  y proovedores, con el objetivo de ir mejorando continuamente y
+                  ser siempre los primeros.
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </section>
+      {/* <section className="container py-5">
         <div className="">
           <Row>
             <Col sm={12} md={6} lg={6} className="g-0">
@@ -74,7 +145,7 @@ const Nosotros = () => {
             </Col>
           </Row>
         </div>
-      </section>
+      </section> */}
     </div>
   );
 };

cdcaac1fa231324047b4898c483db9d30b9b486e
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-10-05 21:44:17 -0300

    Agregué lógica a form reclamos

--- src/components/views/FormularioReclamos.js
+++ src/components/views/FormularioReclamos.js
@@ -11,13 +11,13 @@ const FormularioReclamos = () => {
     e.preventDefault();
 
     const EMAILJS_SERVICE = process.env.REACT_APP_EMAILJS_SERVICE;
-    const EMAILJS_TEMPLATE = process.env.REACT_APP_EMAILJS_TEMPLATE;
+    const EMAILJS_TEMPLATERECLAMOS = process.env.REACT_APP_EMAILJS_TEMPLATERECLAMOS;
     const EMAILJS_PUBLICKEY = process.env.REACT_APP_EMAILJS_PUBLICKEY;
 
     emailjs
       .sendForm(
         EMAILJS_SERVICE,
-        EMAILJS_TEMPLATE,
+        EMAILJS_TEMPLATERECLAMOS,
         refForm.current,
         EMAILJS_PUBLICKEY
       )
@@ -25,14 +25,14 @@ const FormularioReclamos = () => {
         console.log(result.text);
         refForm.current.reset();
         Swal.fire(
-          "Mensaje enviado",
-          "El mensaje será respondido a la brevedad",
+          "Su reclamo a sido enviado exitosamente",
+          "Será respondido a la brevedad",
           "success"
         );
       })
       .catch((error) => {
         console.log(error.text);
-        alert("El mensaje no pudo ser enviado");
+        alert("El reclamo no pudo ser enviado");
       });
   };
 
@@ -40,16 +40,16 @@ const FormularioReclamos = () => {
         <Form ref={refForm} onSubmit={handleSubmit}>
             <Form.Group className="my-4">
                 <Form.Label>Fecha*</Form.Label>
-                <Form.Control type="date" placeholder="" />
+                <Form.Control type="date" placeholder="" name='from-date'/>
                 <Form.Label className="my-2">Producto*</Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder="Nombre Producto"
+                  placeholder="Nombre Producto" name='from-product'
                 />
                 <Form.Label className="my-2">Cantidad*</Form.Label>
-                <Form.Control type="number" placeholder="1" />
+                <Form.Control type="number" placeholder="1" name='from-numbers'/>
                 <Form.Label className="my-2">Motivo*</Form.Label>
-                <Form.Control as="textarea" rows={4} />
+                <Form.Control as="textarea" rows={4} name='from-complain'/>
                 <Form.Label className="my-2">Datos de contacto*</Form.Label>
                   <Form.Control type="text" placeholder="Nombre Apellido" name="from-name" required/>
                   <Form.Label className="my-2">Email*</Form.Label>
@@ -59,7 +59,7 @@ const FormularioReclamos = () => {
                     name="user-email"
                     required
                   />
-                <Button variant="outline-secondary" className="my-3 ms-1">
+                <Button variant="outline-secondary" className="my-3 ms-1" type='submit'>
                   Enviar
                 </Button>
               </Form.Group>
